c8ff6a2fc729e5090f6177dbe27b8237f97efc2e
kehaus <kehaus@protonmail.com>
2021-03-26 00:59:38 +0100

    create a test folder with rudementary test procedures
    This little-develeped test script forms the basis on which future test routines can be built upon. The test cases implemented so far allow to test
    header parameter parsing and txt-to-hdf5 conversion. i will add more test cases in the future.

---
+++ test/test_data_loading.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 23:47:56 2021
+
+@author: kh
+"""
+
+import os
+import sys
+
+# ===========================================================================
+# import mla_reconstruction function
+# ===========================================================================
+repo_dir = os.path.dirname(os.getcwd())
+src_dir = os.path.join(
+    repo_dir,
+    'src'
+)
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+    
+# ===========================================================================
+# load test data
+# ===========================================================================
+data_dir = os.path.join('examples', 'test_data')
+data_subdir = '2021-03-19_grid-MLA-zsweep_5x5nm_5x5pxl_test'
+
+fn_txt = os.path.join(
+    repo_dir, data_dir, data_subdir, 
+    'Measurement of 2021-03-20 0655.txt'
+)
+
+
+# # ===========================================================================
+# # test loead-header function
+# # ===========================================================================
+# from mla_reconstruction import _load_mla_data_header
+
+
+# # ==== Test case 1: load from str (should work)
+# f = open(fn_txt, mode='r')
+# prm = _load_mla_data_header(f, pixel_number=1000)
+# f.close()
+
+# # ==== Test case 2: load from file handler (should work)
+# prm = _load_mla_data_header(fn_txt, pixel_number=1000)
+# f.close()
+
+
+# # ==== Test case 3: load from non-existant file (should raise MLAReconstructionException)
+# prm = _load_mla_data_header('wrong_filename.txt', pixel_number=1000)
+# f.close()
+
+# # ==== Test case 4: load with wrong variable type (should raise TypeError)
+# prm = _load_mla_data_header(17, pixel_number=1000)
+
+
+
+# # ===========================================================================
+# # test parse-header function
+# # ===========================================================================
+# from mla_reconstruction import _load_mla_data, _create_block_list, _parse_mla_data_header
+
+
+# lines = _load_mla_data(fn_txt)
+# block_list = _create_block_list(lines)
+# hd_prm = _parse_mla_data_header(block_list)
+
+
+
+
+# ===========================================================================
+# test txt to hdf5 conversion
+# ===========================================================================
+from mla_reconstruction import _load_mla_data_into_hdf5_dev
+
+# ==== Test case 1: load and reshape
+fn_h5 = _load_mla_data_into_hdf5_dev(
+    fn_txt,
+    resize_curr=(5,5,40,119),
+    resize_cond=(5,5,40,119),
+    pixel_number=1000,
+    zsweep_nr=40,
+    mode='w',
+    # missing_pxls=[10,60,135]
+)
+
+
+
+

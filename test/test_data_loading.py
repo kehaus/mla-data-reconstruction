b063e446df6ce88f0da230386ffec9bced848f64
kehaus <kehaus@protonmail.com>
2021-06-06 16:06:08 +0200

    add more test data; clean up test-cases

--- test/test_data_loading.py
+++ test/test_data_loading.py
@@ -9,9 +9,9 @@ Created on Thu Mar 25 23:47:56 2021
 import os
 import sys
 
-# ===========================================================================
-# import mla_reconstruction function
-# ===========================================================================
+### =========================================================================
+#%% import mla_reconstruction function
+### =========================================================================
 repo_dir = os.path.dirname(os.getcwd())
 src_dir = os.path.join(
     repo_dir,
@@ -20,21 +20,26 @@ src_dir = os.path.join(
 if src_dir not in sys.path:
     sys.path.append(src_dir)
     
-# ===========================================================================
-# load test data
-# ===========================================================================
+### =========================================================================
+#%% load test data
+### =========================================================================
 data_dir = os.path.join('examples', 'test_data')
-data_subdir = '2021-03-19_grid-MLA-zsweep_5x5nm_5x5pxl_test'
-
-fn_txt = os.path.join(
-    repo_dir, data_dir, data_subdir, 
+data_subdir1 = '2021-03-19_grid-MLA-zsweep_5x5nm_5x5pxl_test'
+fn_txt1 = os.path.join(
+    repo_dir, data_dir, data_subdir1, 
     'Measurement of 2021-03-20 0655.txt'
 )
 
+data_subdir2 = '20210604_MLA_DCoffset_test'
+fn_txt2 = os.path.join(
+    repo_dir, data_dir, data_subdir2,
+    'Measurement of 2021-06-04 1734.txt'
+)
 
-# # ===========================================================================
-# # test loead-header function
-# # ===========================================================================
+
+# ### =========================================================================
+# #%% test loead-header function
+# ### =========================================================================
 # from mla_reconstruction import _load_mla_data_header
 
 
@@ -57,9 +62,9 @@ fn_txt = os.path.join(
 
 
 
-# # ===========================================================================
-# # test parse-header function
-# # ===========================================================================
+# ### =========================================================================
+# #%% test parse-header function
+# ### =========================================================================
 # from mla_reconstruction import _load_mla_data, _create_block_list, _parse_mla_data_header
 
 
@@ -70,14 +75,14 @@ fn_txt = os.path.join(
 
 
 
-# ===========================================================================
-# test txt to hdf5 conversion
-# ===========================================================================
-from mla_reconstruction import _load_mla_data_into_hdf5_dev
+### =========================================================================
+#%% test txt to hdf5 conversion -- Non offset
+### =========================================================================
+from mla_reconstruction import _load_mla_data_into_hdf5
 
 # ==== Test case 1: load and reshape
-fn_h5 = _load_mla_data_into_hdf5_dev(
-    fn_txt,
+fn_h5 = _load_mla_data_into_hdf5(
+    fn_txt1,
     resize_curr=(5,5,40,119),
     resize_cond=(5,5,40,119),
     pixel_number=1000,
